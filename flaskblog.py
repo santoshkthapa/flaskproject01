bdec66bceb24ca27fb14a21ce45b67f35af3126e
Santosh Kumar Thapa <santoshkumar.thapa@digital.cabinet-office.gov.uk>
2019-10-13 16:05:39 +0100

    working on error handling

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -37,9 +38,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data =='admin@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__== '__main__':
