d0356e363008f4ccfc96e8934c14c609849e06ea
Santosh Kumar Thapa <santoshkumar.thapa@digital.cabinet-office.gov.uk>
2019-10-13 16:49:22 +0100

    creating database

--- flaskblog.py
+++ flaskblog.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 posts = [
     {
         'author':'santosh kumar thapa',
